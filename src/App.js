ea148aaa0e76e602739e8543d338b10e89d75586
Rananjaya <34897947+Rananjaya@users.noreply.github.com>
2020-04-23 15:19:43 +0530

    Make project stru and add com

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Cards,Chart,ContryPicker} from './Components';
+import Styles from './App.module.css';
+
 
 class App extends React.Component{
     render(){
         return(
-            <div>
-                <h1>App</h1> 
+            <div className={Styles.container}>
+               <Cards />
+               <ContryPicker />
+               <Chart />
             </div>
         )
     }
---
+++ src/Components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ConutryPicker = () =>{
+    return (
+        <h1>ConutryPicker</h1>
+    )
+}
+
+export default ConutryPicker;
